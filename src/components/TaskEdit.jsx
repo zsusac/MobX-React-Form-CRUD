937fd24e64918c7c75af229703e746e9da8dd182
Zvonimir Susac <susac.zvonimir@gmail.com>
2017-03-27 09:51:18 +0200

    Move form markup to new component

--- src/components/TaskEdit.jsx
+++ src/components/TaskEdit.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {inject, observer, action} from 'mobx-react';
 import TaskFormStore from '../stores/TaskFormStore.jsx';
-import MobxReactFormDevTools from 'mobx-react-form-devtools';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
+import TaskForm from './forms/TaskForm.jsx';
 
 @inject('TaskFormStore')
 @observer
@@ -11,79 +11,27 @@ class TaskEdit extends Component {
     constructor(props) {
         super(props);
         const {TaskFormStore} = this.props;
-        // Reset the Form to default values.	
+        // Reset the TaskForm to default values.
         TaskFormStore.reset();
+        // Fetch task and populate TaskForm with task data
         TaskFormStore.fetchById(this.props.params.id);
     }
 
-    render () {
-        const {TaskFormStore} = this.props;
+    render() {
 
-        MobxReactFormDevTools.register({TaskFormStore});
-        // select form to show into the devtools
-        MobxReactFormDevTools.select('TaskFormStore');
-
-        // open the devtools (closed by default)
-        MobxReactFormDevTools.open(true);
-
-        let onDelete = TaskFormStore.deleteTask.bind(this, TaskFormStore.values());
-        
         return (
-                <div>
-                    <form onSubmit={TaskFormStore.onSubmit}>
-                        <label
-                            htmlFor={TaskFormStore
-                            .$('task')
-                            .id}>
-                            {TaskFormStore
-                                .$('task')
-                                .label}
-                        </label>
-                        <input
-                            {...TaskFormStore.$('task').bind({ type: 'textbox' })}
-                            value={TaskFormStore
-                            .$('task')
-                            .value}/>
-                        <p>{TaskFormStore
-                                .$('task')
-                                .error}</p>
-
-                        <label
-                            htmlFor={TaskFormStore
-                            .$('completed')
-                            .id}>
-                            {TaskFormStore
-                                .$('completed')
-                                .label}
-                        </label>
-                        <input
-                            {...TaskFormStore.$('completed').bind({ type: 'checkbox' })}
-                            checked={TaskFormStore
-                            .$('completed')
-                            .value}
-                            className="left"/>
-                        <p>{TaskFormStore
-                                .$('completed')
-                                .error}</p>
-
-                        <button type="submit" onClick={TaskFormStore.onSubmit}>Submit</button>
-                        <button type="button" onClick={TaskFormStore.onClear}>Clear</button>
-                        <button type="button" onClick={TaskFormStore.onReset}>Reset</button>
-                        <button type="button" onClick={onDelete}>Delete</button>
-                        <p>{TaskFormStore.error}</p>
-                    </form>
-
-                    <Link to={'/'}>Back</Link>
-
-
-                    <MobxReactFormDevTools.UI/>          
-                </div>
+            <div>
+                <TaskForm/>
+                <Link to={'/'}>Back</Link>
+            </div>
         )
     }
 }
 
 TaskEdit.propTypes = {
-    TaskFormStore: React.PropTypes.objectOf(TaskFormStore)
+    TaskFormStore: React
+        .PropTypes
+        .objectOf(TaskFormStore)
 }
 
 export default TaskEdit
---
+++ src/components/forms/TaskForm.jsx
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {inject, observer} from 'mobx-react';
+import TaskFormStore from '../../stores/TaskFormStore.jsx';
+import MobxReactFormDevTools from 'mobx-react-form-devtools';
+
+@inject('TaskFormStore')
+@observer
+class TaskForm extends Component {
+    render() {
+        const {TaskFormStore} = this.props;
+
+        MobxReactFormDevTools.register({TaskFormStore});
+        // select form to show into the devtools
+        MobxReactFormDevTools.select('TaskFormStore');
+        // open the devtools (closed by default)
+        MobxReactFormDevTools.open(true);
+
+        let onDelete = TaskFormStore
+            .deleteTask
+            .bind(this, TaskFormStore.values());
+
+        return (
+            <form onSubmit={TaskFormStore.onSubmit}>
+                <label
+                    htmlFor={TaskFormStore
+                    .$('task')
+                    .id}>
+                    {TaskFormStore
+                        .$('task')
+                        .label}
+                </label>
+                <input
+                    {...TaskFormStore.$('task').bind({ type: 'textbox' })}
+                    value={TaskFormStore
+                    .$('task')
+                    .value}/>
+                <p>{TaskFormStore
+                        .$('task')
+                        .error}</p>
+
+                <label
+                    htmlFor={TaskFormStore
+                    .$('completed')
+                    .id}>
+                    {TaskFormStore
+                        .$('completed')
+                        .label}
+                </label>
+                <input
+                    {...TaskFormStore.$('completed').bind({ type: 'checkbox' })}
+                    checked={TaskFormStore
+                    .$('completed')
+                    .value}
+                    className="left"/>
+                <p>{TaskFormStore
+                        .$('completed')
+                        .error}</p>
+
+                <button type="submit" onClick={TaskFormStore.onSubmit}>Submit</button>
+                <button type="button" onClick={TaskFormStore.onClear}>Clear</button>
+                <button type="button" onClick={TaskFormStore.onReset}>Reset</button>
+                <button type="button" onClick={onDelete}>Delete</button>
+                <p>{TaskFormStore.error}</p>
+                <MobxReactFormDevTools.UI/>
+            </form>
+        )
+    }
+}
+
+TaskForm.propTypes = {
+    TaskFormStore: React
+        .PropTypes
+        .objectOf(TaskFormStore)
+}
+
+export default TaskForm
